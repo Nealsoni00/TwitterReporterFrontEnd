57b759e1738b473b1d4579d07ec0c13d60984d44
Neal Soni <nealsoni00@gmail.com>
2020-04-23 23:54:52 -0400

    added prompt to register for notifications

--- TwitterReporteriOS/TwitterReporteriOS/FirstViewController.swift
+++ TwitterReporteriOS/TwitterReporteriOS/FirstViewController.swift
@@ -13,8 +13,26 @@ class FirstViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        registerForRemoteNotification()
     }
 
+    func registerForRemoteNotification() {
+        if #available(iOS 10.0, *) {
+            let center  = UNUserNotificationCenter.current()
 
+            center.requestAuthorization(options: [.sound, .alert, .badge]) { (granted, error) in
+                if error == nil{
+                    DispatchQueue.main.async {
+                        UIApplication.shared.registerForRemoteNotifications()
+                    }
+                }
+            }
+
+        }
+        else {
+            UIApplication.shared.registerUserNotificationSettings(UIUserNotificationSettings(types: [.sound, .alert, .badge], categories: nil))
+            UIApplication.shared.registerForRemoteNotifications()
+        }
+    }
 }
 
