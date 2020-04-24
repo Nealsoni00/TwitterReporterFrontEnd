166466278dbac0e7c7bcdf831daf897799ebddc4
Neal Soni <nealsoni00@gmail.com>
2020-04-23 20:35:40 -0400

    removed pods from git

--- TwitterReporteriOS/Pods/Target Support Files/Pods-TwitterReporteriOS/Pods-TwitterReporteriOS-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_TwitterReporteriOS : NSObject
-@end
-@implementation PodsDummy_Pods_TwitterReporteriOS
-@end
--- TwitterReporteriOS/Pods/Target Support Files/Pods-TwitterReporteriOSTests/Pods-TwitterReporteriOSTests-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_TwitterReporteriOSTests : NSObject
-@end
-@implementation PodsDummy_Pods_TwitterReporteriOSTests
-@end
