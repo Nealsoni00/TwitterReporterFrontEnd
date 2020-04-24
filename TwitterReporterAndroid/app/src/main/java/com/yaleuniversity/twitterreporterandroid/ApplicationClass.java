0dbadc56fa5256c410446633892fbc8ab6ac0497
Neal Soni <nealsoni00@gmail.com>
2020-04-24 00:13:56 -0400

    initialized app to recieve notifications using one signal id

---
+++ TwitterReporterAndroid/app/src/main/java/com/yaleuniversity/twitterreporterandroid/ApplicationClass.java
@@ -0,0 +1,19 @@
+package com.yaleuniversity.twitterreporterandroid;
+
+import android.app.Application;
+
+import com.onesignal.OneSignal;
+
+public class ApplicationClass extends Application {
+    @Override
+    public void onCreate() {
+        super.onCreate();
+
+        // OneSignal Initialization
+        OneSignal.startInit(this)
+                .inFocusDisplaying(OneSignal.OSInFocusDisplayOption.Notification)
+                .unsubscribeWhenNotificationsAreDisabled(true)
+                .init();
+    }
+
+}
