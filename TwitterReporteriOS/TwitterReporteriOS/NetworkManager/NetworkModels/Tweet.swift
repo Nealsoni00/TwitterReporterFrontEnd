27de91519b44aee47c792430745ddc1080e50067
Neal Soni <nealsoni00@gmail.com>
2020-04-30 20:48:19 -0400

    moya-affied the tweet struct to be type safe and decodable form json

--- TwitterReporteriOS/TwitterReporteriOS/NetworkManager/NetworkModels/Tweet.swift
+++ TwitterReporteriOS/TwitterReporteriOS/NetworkManager/NetworkModels/Tweet.swift
@@ -9,23 +9,36 @@
 import Foundation
 
 struct Tweet {
-    var profileImageURL: String!
-    var nameOfPerson: String!
-    var handle: String!
-    var time: String!
-    var textOfTweet: String!
-    var likesCount: String!
-    var retweetCount: String!
-    var tag: String!
-    
-    init(profileImageURL: String, nameOfPerson: String, handle: String, time: String, textOfTweet: String, likesCount: String, retweetCount: String, tag: String) {
-        self.profileImageURL = profileImageURL
-        self.nameOfPerson = nameOfPerson
-        self.handle = handle
-        self.time = time
-        self.textOfTweet = textOfTweet
-        self.likesCount = likesCount
-        self.retweetCount = retweetCount
-        self.tag = tag
+    let profileImageURL: String
+    let nameOfPerson: String
+    let handle: String
+    let time: String
+    let textOfTweet: String
+    let likesCount: String
+    let retweetCount: String
+    let tag: String
+}
+
+extension Tweet: Decodable {
+    enum TweetCodingKeys: String, CodingKey {
+        case profileImageURL = "profile_image_url"
+        case nameOfPerson = "author"
+        case handle
+        case time
+        case textOfTweet = "text_of_tweet"
+        case likesCount = "likes_count"
+        case retweetCount = "retweet_count"
+        case tag
+    }
+    init(from decoder: Decoder) throws {
+        let container = try decoder.container(keyedBy: TweetCodingKeys.self)
+        profileImageURL = try container.decode(String.self, forKey: .profileImageURL)
+        nameOfPerson = try container.decode(String.self, forKey: .nameOfPerson)
+        handle = try container.decode(String.self, forKey: .handle)
+        time = try container.decode(String.self, forKey: .time)
+        textOfTweet = try container.decode(String.self, forKey: .textOfTweet)
+        likesCount = try container.decode(String.self, forKey: .likesCount)
+        retweetCount = try container.decode(String.self, forKey: .retweetCount)
+        tag = try container.decode(String.self, forKey: .tag)
     }
 }
