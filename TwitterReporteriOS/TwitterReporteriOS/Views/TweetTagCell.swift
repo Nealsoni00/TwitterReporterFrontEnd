7d0b411ab1fd2567c3ce6967dee86f95ae04b050
Neal Soni <nealsoni00@gmail.com>
2020-05-01 11:56:12 -0400

    added another tab testing and attempted to fix reference again to no avail

---
+++ TwitterReporteriOS/TwitterReporteriOS/Views/TweetTagCell.swift
@@ -0,0 +1,13 @@
+//
+//  TweetTagCell.swift
+//  TwitterReporteriOS
+//
+//  Created by Neal Soni on 4/30/20.
+//  Copyright © 2020 Yale University. All rights reserved.
+//
+
+import UIKit
+
+class TweetTagCell: UICollectionViewCell {
+    
+}
---
+++ TwitterReporteriOS/TwitterReporteriOS/Views/TweetTagsCollectionView.swift
@@ -0,0 +1,21 @@
+//
+//  TweetTagsCollectionView.swift
+//  TwitterReporteriOS
+//
+//  Created by Neal Soni on 4/30/20.
+//  Copyright © 2020 Yale University. All rights reserved.
+//
+
+import UIKit
+
+class TweetTagsCollectionView: UICollectionView {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
