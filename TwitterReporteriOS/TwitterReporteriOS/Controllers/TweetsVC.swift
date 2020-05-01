73e222b5f60ae667f20e2879b76fe0cc8ab93e70
Neal Soni <nealsoni00@gmail.com>
2020-04-30 23:09:58 -0400

    intialize cells with data

--- TwitterReporteriOS/TwitterReporteriOS/Controllers/TweetsVC.swift
+++ TwitterReporteriOS/TwitterReporteriOS/Controllers/TweetsVC.swift
@@ -30,30 +30,34 @@ class TweetsVC: UITableViewController {
         networkProvider.getTweets() { tweets in
             self.tweets = tweets
             print(tweets)
+            self.tableView.reloadData()
         }
+//        self.tableView.register(TweetCell.self,  forCellReuseIdentifier: "tweetCell123")
+
     }
 
     // MARK: - Table view data source
 
     override func numberOfSections(in tableView: UITableView) -> Int {
         // #warning Incomplete implementation, return the number of sections
-        return 0
+        return 1
     }
 
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         // #warning Incomplete implementation, return the number of rows
-        return 0
+        return tweets.count
     }
 
-    /*
+    
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        let cell = tableView.dequeueReusableCell(withIdentifier: "reuseIdentifier", for: indexPath)
+        print("HERE!!!!")
+        let cell = tableView.dequeueReusableCell(withIdentifier: "cell", for: indexPath) as! TweetCell
 
-        // Configure the cell...
+        cell.initialize(tweet: tweets[indexPath.row])
 
         return cell
     }
-    */
+    
 
     /*
     // Override to support conditional editing of the table view.
